675013fd5dcb19b2c5808394c2461aecbd2bbc3d
edupxavier <49204455+edupxavier@users.noreply.github.com>
2021-10-14 12:13:28 -0300

    benchmark for comparing different implementations

---
+++ btb1/btb.Benchmarks/Program.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using core;
+using System;
+
+namespace btb.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BitVersusBoolean>();
+        }
+    }
+
+    public class BitVersusBoolean
+    {
+        [GlobalSetup]
+        public void Setup()
+        {
+        }
+
+        public bool BooleanAssign(bool inputA, bool inputB)
+        {
+            return inputA == inputB;
+        }
+
+        public bool BitAssign(Bit inputA, Bit inputB)
+        {
+            return inputA == inputB;
+        }
+
+        [Params(false, true, false, true)]
+        public bool A { get; set; }
+
+        [Params(false, false, true, true)]
+        public bool B { get; set; }
+
+        [Benchmark]
+        public void bBool() => BooleanAssign(A, B);
+
+        [Benchmark]
+        public void bBit() => BitAssign(new Bit(A), new Bit(B));
+
+    }
+
+}
